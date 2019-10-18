258498b396fec2ec80828155bcd0491c2940484c
GenaroBarrera <lvg821@my.utsa.edu>
2019-10-17 22:33:18 -0500

    updated chase script in test branch

--- Assets/Scripts/chase.cs
+++ Assets/Scripts/chase.cs
@@ -5,6 +5,81 @@ using UnityEngine;
 public class chase : MonoBehaviour
 {
     public Transform player;
+    public Transform head;
+    static Animator anim;
+
+    string state = "patrol";
+    public GameObject[] waypoints;
+    int currentWP = 0;
+    public float rotSpeed = 0.2f;
+    public float speed = 1.5f;
+    float accuracyWP = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - this.transform.position;
+        direction.y = 0;
+        float angle = Vector3.Angle(direction, head.up);
+
+        if (state == "patrol" && waypoints.Length > 0)
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            if (Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
+            {
+                //To randomize the waypoints the enemy goes to next
+                //currentWP = Random.Range(0, waypoints.Length);
+
+                currentWP++;
+                if (currentWP >= waypoints.Length)
+                {
+                    currentWP = 0;
+                }
+            }
+
+            //rotate towards waypoint
+            direction = waypoints[currentWP].transform.position - transform.position;
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+            this.transform.Translate(0, 0, Time.deltaTime * speed);
+        }
+
+        if (Vector3.Distance(player.position, this.transform.position) < 10 && (angle < 30 || state == "pursuing"))
+        {
+            state = "pursuing";
+            this.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+
+            if (direction.magnitude > 5)
+            {
+                this.transform.Translate(0, 0, Time.deltaTime * speed);
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAttacking", false);
+            }
+            else
+            {
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isWalking", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isAttacking", false);
+            state = "patrol";
+        }
+    }
+}
+
+/* (old script)
+public class chase : MonoBehaviour
+{
+    public Transform player;
     static Animator anim;
 
     // Start is called before the first frame update
@@ -23,8 +98,8 @@ public class chase : MonoBehaviour
         {
             direction.y = 0;
 
-            //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), .1f * Time.deltaTime);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), .1f * Time.deltaTime);
 
             anim.SetBool("isIdle", false);
             if(direction.magnitude > 5)
@@ -47,3 +122,4 @@ public class chase : MonoBehaviour
         }
     }
 }
+*/
