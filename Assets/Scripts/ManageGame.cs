4ad77330e59d19ba5eff1f5b31a35cbd6a3f08f1
Brenden <jyrcht@gmail.com>
2019-10-17 21:27:08 -0500

    Hopefully this adds what I worked on and no breaking

---
+++ Assets/Scripts/ManageGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageGame : MonoBehaviour
+{
+
+  public GameObject start;
+  public GameObject player = null;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    start = GameObject.FindWithTag("Start");
+    
+    if (SceneManager.GetActiveScene().name == "Master")
+      SceneManager.SetActiveScene(SceneManager.GetSceneByName("Title"));
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (player == null && SceneManager.GetActiveScene().name == "Outside")
+    {
+      player = GameObject.FindWithTag("Player");
+    }
+    if (player != null)
+    {
+      print("Found~!");
+    }
+
+    if (start != null)
+    {
+      //print(start.gameObject.GetComponent<StartGame>().GetChange());
+      if (start.gameObject.GetComponent<StartGame>().GetChange())
+      {
+        ChangeScene("Outside");
+      }
+    }
+
+  }
+
+    public void ChangeScene(string name)
+    {
+      if(SceneManager.GetActiveScene().name == "Title")
+        SceneManager.UnloadSceneAsync(sceneName: "Title");
+      SceneManager.LoadSceneAsync(sceneName: name);
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+
+  private bool changeScene = false;
+  private RaycastHit hit;
+
+  // Start is called before the first frame update
+  void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      //print(gameObject.GetComponent<Button>().);
+      OVRInput.Update();
+      if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+      {
+        Debug.Log("Button Down");
+         SetChange(true);
+      }
+    }
+
+  public void SetChange(bool val)
+  {
+    changeScene = val;
+  }
+  public bool GetChange()
+  {
+    return changeScene;
+  }
+
+}
