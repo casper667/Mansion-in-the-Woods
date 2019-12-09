34e9cc1821e65e42ed25503bca03ae60f435b424
casper667 <ach4852@msn.com>
2019-12-09 04:37:35 -0600

    Added pause menu to inside scene

---
+++ Assets/Scripts/MyAudioManagerSettings.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MyAudioManagerSettings : MonoBehaviour
+{
+    public static MyAudioManagerSettings instance;
+    public AudioMixer mixer;
+    public MyAudioSettings[] audioSettings;
+    private enum AudioGroups { Master, Music, SFX };
+
+    void Awake()
+    {
+        instance = GetComponent<MyAudioManagerSettings>();
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < audioSettings.Length; i++)
+        {
+            audioSettings[i].Initialize();
+        }
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        audioSettings[(int)AudioGroups.Master].SetExposedParam(value);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        audioSettings[(int)AudioGroups.Music].SetExposedParam(value);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        audioSettings[(int)AudioGroups.SFX].SetExposedParam(value);
+    }
+}
+
+[System.Serializable]
+public class MyAudioSettings
+{
+    public Slider slider;
+    public string exposedParam;
+
+    public void Initialize()
+    {
+        slider.value = PlayerPrefs.GetFloat(exposedParam);
+    }
+
+    public void SetExposedParam(float value)
+    {
+        MyAudioManagerSettings.instance.mixer.SetFloat(exposedParam, value);
+        PlayerPrefs.SetFloat(exposedParam, value);
+    }
+}
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject gameUI;
+    public GameObject pauseMenuUI;
+    public GameObject optionsMenuUI;
+    public GameObject fadedBackground;
+
+    private bool IsPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(false);
+        fadedBackground.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        gameUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(false);
+        fadedBackground.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void Pause()
+    {
+        gameUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        fadedBackground.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
